aff1525cff1bbedfb6a7a9b3a93d82f51e0a84ff
nathan lopez <cherrygal2011@hotmail.com>
2015-01-09 14:58:57 -0800

    doing well

--- public_html/Script.js
+++ public_html/Script.js
@@ -1,5 +1,21 @@
 
 $ ("document").ready(function(){
-$(".MTGzSquad").css("background-color", "yellow");
-
+  //  $(".MTGzSquad").css("background-color", "yellow");
+//    $("div p:last-child").css("background-color", "cyan");
+//    $("div p:nth-child(1)").css("background-color", "cyan");
+    $('p:gt(1)').css('border-style', 'solid');
+    $('p:first').css({'background-color':'cyan', 'color':'white'});
+    $("h1").bind('mouseover',mouseOverMe).bind('mouseout',mouseOutMe); 
+    $('h1').bind('click',mouseclick); 
 });
+
+function mouseOverMe(){
+    $("h1").html("this  is easy suker");
+}
+function mouseOutMe (){
+    $('h1').html('MTGz Squad');
+}
+
+function mouseclick(){
+    $('p').html('tryrteytryteytyeyetyteytrytrytrytrytr');
+}
