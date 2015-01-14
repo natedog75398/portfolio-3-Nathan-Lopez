a6c5ab6d2dc4892de633dc148ff9e54043799ca3
nathan lopez <cherrygal2011@hotmail.com>
2015-01-14 15:01:25 -0800

    doing very well

--- public_html/Script.js
+++ public_html/Script.js
@@ -1,28 +1,50 @@
-
-$ ("document").ready(function(){
-  //  $(".MTGzSquad").css("background-color", "yellow");
-//    $("div p:last-child").css("background-color", "cyan");
-//    $("div p:nth-child(1)").css("background-color", "cyan");
-    $('p:gt(1)').css('border-style', 'solid');
-    $('p:first').css({'background-color':'cyan', 'color':'white'});
-    $("h1").bind('mouseover',mouseOverMe).bind('mouseout',mouseOutMe); 
-    $('h1').bind('click',mouseclick); 
-    $('#replaceWText').bind('click', replaceWText);
-    $('#randpara').bind('click', removeApara);
+$("document").ready(function(){
+    $('.yeah').css('background-color', 'blue');
     
-});
+    $('div:last p:nth-child(3)').css('background-color','pink');
+
+    $('div:last p:nth-child(4)').css('background-color','red');
+    
+    $('div:last p:nth-child(5)').css('background-color','white');
+    
+    $('div:last p:nth-child(3)').css('border-style','solid');
+    $('div:last p:nth-child(4)').css('border-style','solid');
+    $('div:last p:nth-child(5)').css('border-style','solid');
+    
+    
+   $('h1').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe)
+    
+    $("h1").bind('click', mouseClick);
+    
+    $('#replcaementWText').bind('click', replaceWText);
+    
+    $('#randPara').bind('click', addAPara);
+    
+    $('#removepara').bind('click', removeAPara);
+});     
+
+function removeAPara(){
+    $ ('#randPara p:last').remove();
+}
+
+function addAPara(){
+    $('randPara').append('<p>ADDED</p>');
+}
+
+function replaceWText(){
+    $('#replaceWtext').text('replaced');
+}
+
 
 function mouseOverMe(){
-    $("h1").html("this  is easy suker");
-}
-function mouseOutMe (){
-    $('h1').html('MTGz Squad');
+    $("h1").html("do not click on these words");
 }
 
-function mouseclick(){
-    $('p').html('tryrteytryteytyeyetyteytrytrytrytrytr');
+function mouseOutMe(){
+    $('h1').html('this is easy');
 }
-function replaceWtext(){
-    $('#replaceWText').'text'('replaced!');
+
+function mouseClick(){
+    $('p').html('bruh;MTGz is lifeeeee');
 }
- 
+
